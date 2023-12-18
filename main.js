3d369b2778d265a3923a34efbf246474af96ca99
FilipFE23 <filip.bockarovski@gritacademy.se>
2023-12-18 15:08:13 +0100

    commit

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import { fetchTmdb } from './module/fetchAPI.js';
 
-fetchTmdb(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&api_key=`)
+const form = document.querySelector('form');
+form.addEventListener('click', event => {
+    event.preventDefault();
+    if(event.target.id == 'popularBtn'){
+        let cat = fetchTmdb(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=`).then();
+        console.log(cat);
+    }
+    else if(event.target.id == 'topRatedBtn'){
+        fetchTmdb(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&api_key=`);
+    }
+});
--- module/fetchAPI.js
+++ module/fetchAPI.js
@@ -1,12 +1,22 @@
 const API_KEY = 'e260aeb962197fce7f389cbc57692172';
 
-function fetchTmdb(urlApi){
+async function fetchTmdb(urlApi){
 const url = urlApi + API_KEY;
 
-fetch(url)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("error:" + err));
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((json) => {
+//     return json;
+//   })
+//   .catch((err) => console.error("error:" + err));
+try {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.results;
+}
+catch(err){
+  console.error("error:" + err)
+};
 };
 
 export{fetchTmdb};
